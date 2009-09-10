31ec28089887140df4704b45db941c091efa7caf
Sébastien Mosser <sebastien.mosser@gmail.com>
2009-09-10 17:42:24 +0000

    cleaning up repository content

--- www/config/local.conf.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('ADMIN_USER',"admin");
-define('ADMIN_PASS',"admin");
-define('BROKEN_GLASS',true);
-define ('LDAP_SERVER', "ldaps://godot.polytech.unice.fr");
-define("LDAP_ROOT","dc=polytech,dc=unice,dc=fr");
-ini_set('date.timezone','Europe/Berlin');
-?>
